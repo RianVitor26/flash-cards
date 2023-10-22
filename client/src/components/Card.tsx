d3e77d34780292dcaf840a8502c3c7d5a9ca319f
RianVitor26 <rianvitorlhe@gmail.com>
2023-10-22 20:28:06 -0300

    feat: definindo load dos cards

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -10,10 +10,10 @@ export const Card = ({ term, translation }: ICardsProps) => {
 
     return (
         <div className={`card w-52 h-40 relative ${isActive ? 'flip' : ''}`} onClick={flipCardAnimation}>
-            <div className={`absolute front bg-gray-50 text-black rounded-md flex justify-center items-center font-black shadow-xl hover:shadow-2xl transition-all duration-300 cursor-pointer ${isActive ? 'hidden' : ''}`}>
+            <div className={`absolute front bg-gray-50 text-black rounded-md flex justify-center text-center items-center font-black shadow-xl hover:shadow-2xl transition-all duration-300 cursor-pointer ${isActive ? 'hidden' : ''}`}>
                 {term}
             </div>
-            <div className={`absolute back bg-rose-500 text-gray-50 rounded-md flex justify-center items-center font-black shadow-xl hover:shadow-2xl transition-all duration-300 cursor-pointer ${isActive ? '' : 'hidden'}`}>
+            <div className={`absolute back bg-rose-500 text-gray-50 rounded-md flex justify-center text-center items-center font-black shadow-xl hover:shadow-2xl transition-all duration-300 cursor-pointer ${isActive ? '' : 'hidden'}`}>
                 {translation}
             </div>
         </div>  
